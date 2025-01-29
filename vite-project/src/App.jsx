45e466fc42bb64af83e46b4ee0961deafa52ac65
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-29 12:07:28 +0330

    routing & main components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navbar, Feed, CardDetail, SearchFeed } from './components'
 
-const App = () => {
-  return (
-    <div>App</div>
+const App = () => (
+  
+    <BrowserRouter>
+    <div className='bg-slate-200'>
+    <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Feed/>}/>
+        <Route path='/card/:id' element={<CardDetail/>}/>
+        <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+      </Routes>
+    </div>
+    </BrowserRouter>
   )
-}
 
 export default App
---
+++ vite-project/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Feed } from './Feed'
+export { default as Navbar } from './Navbar'
+export { default as CardDetail } from './CardDetail.jsx'
+export { default as SearchFeed } from './SearchFeed.jsx'
