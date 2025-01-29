e12145ce5ce4ab7a3c035f151c5e161ac33598e7
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-29 12:12:33 +0330

    first touch of feed
    The writings on the One Ring!

--- vite-project/src/components/Feed.jsx
+++ vite-project/src/components/Feed.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <div>
+        <div>
+        <h1>Ash nazg durbatulûk, ash nazg gimbatul, ash nazg thrakatulûk agh burzum-ishi krimpatu</h1>
+        <h2>One Ring to find them, One Ring to bring them all and in the darkness bind them</h2>
+        </div>
+
+    </div>
   )
 }
 
