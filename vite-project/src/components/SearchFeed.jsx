ea520637f87fd532c9c434a3c6655d6b04d0ce21
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-02-04 10:23:26 +0330

    searchFeed

--- vite-project/src/components/SearchFeed.jsx
+++ vite-project/src/components/SearchFeed.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const SearchFeed = () => {
+
+  const [characterData, setCharacterData] = useState([])
+  const { searchItems } = useParams()
+  const [loading, setLoading] = useState(false)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  const fetchFromAPI = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch(`https://lotrapi.co/api/v1/characters/${searchItems}`)
+      const data = await response.json()
+      console.log(data)
+      if(data && data.results) {
+        setCharacterData(data.results)
+        setLoading(false) 
+      }
+    } catch (error) {
+      setLoading(false)
+      setErrorMsg(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchFromAPI()
+  }, [searchItems])
+
+  if(loading === true) {
+    <div>Loading...please wait</div>
+  }
+
+  if (errorMsg) {
+    <div>Error...{errorMsg}</div>
+  }
+
   return (
-    <div>SearchFeed</div>
+    <div>
+      {characterData.map((item, index) => (
+          <div key={index}>
+              <h1>{characterData.name}</h1>
+              <p>{characterData.gender}</p>
+              <p>{characterData.weapons}</p>
+          </div>
+      ))}
+    </div>
   )
 }
 
